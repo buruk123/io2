716961a83f821f1d83fd60773a3c4d51d97b18e7
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2019-11-25 20:05:06 +0100

    Doing front

--- bank/index.php
+++ bank/index.php
@@ -13,201 +13,151 @@
 </head>
 <body>
 <?php require_once('topmenu.php') ?>
-<div class="bd-example">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="resources/images/1slider.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Nasz Bank</h5>
-                    <p>Załóż konto już dziś</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="resources/images/2slider.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 style="color: black;">Konta</h5>
-                    <p style="color: black;">Konta dopasowane do Twoich potrzeb</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="resources/images/3slider.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 style="color: black;">Karty</h5>
-                    <p style="color: black;">Duży wybór kart</p>
-                </div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-</div>
-
-<!--TODO: MENU-->
 
 <div class="container">
-
-    <h2 class="my-4">Wiadomości</h2>
-
-    <!-- Marketing Icons Section -->
     <div class="row">
-        <div class="col-lg-4 mb-4">
-            <div class="card h-100">
-                <h4 class="card-header"><b>Przerwa 06.06</b></h4>
-                <div class="card-body">
-                    <p class="card-text">Informujemy, że w dniu 06 czerwca nastąpi tymczasowa przerwa w działaniu
-                        banku
-                        w związku z przeprowadzaną konserwacją. Za wszystkie nieugodnienia przepraszamy.</p>
+        <div class="col col-8">
+            <!--            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et-->
+            <!--            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex-->
+            <!--            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat-->
+            <!--            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit-->
+            <!--            anim id est laborum."-->
+            <!---->
+            <!--            Fragment 1.10.32 z "de Finibus Bonorum et Malorum", napisanej przez Cycerona w 45 r.p.n.e.-->
+            <!--            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem-->
+            <!--            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.-->
+            <!--            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni-->
+            <!--            dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor-->
+            <!--            sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore-->
+            <!--            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis-->
+            <!--            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in-->
+            <!--            ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas-->
+            <!--            nulla pariatur?"-->
+            <div class="row">
+                <div class="col">
+                    <div class="row-custom-center">
+                        <a href=""><img class="onhover" src="resources/images/phones/Apple-iPhoneXr-Black-1-3x.png"
+                                        width="100" height="150"></a><br/>
+                        SRAJFON XR XDDDD
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row-custom-center">
+                        <a href=""><img class="onhover" src="resources/images/phones/note10.png" width="150"
+                                        height="150"></a><br/>
+                        SZMAJSUNK SMILITON XDDDD
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row-custom-center">
+                        <a href=""><img class="onhover" src="resources/images/phones/lgk40.png" width="250"
+                                        height="150"></a><br/>
+                        LGK40
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="row-custom-center">
+                        <a href=""><img class="onhover" src="resources/images/laptops/tufgaming.png" width="180"
+                                        height="150"></a><br/>
+                        tuf gaming 505ge
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row-custom-center">
+                        <a href=""><img class="onhover" src="resources/images/laptops/asusvivobook.png" width="200"
+                                        height="150"></a><br/>
+                        vivobookasus
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row-custom-center">
+                        <a href=""><img class="onhover" src="resources/images/laptops/makbook.png" width="250"
+                                        height="150"></a><br/>
+                        makbook
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="row-custom-center ">
+                        <a href=""><img class="onhover" src="resources/images/software/windows10.png" width="150"
+                                        height="150"></a><br/>
+                        windowzbeniz
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row-custom-center ">
+                        <a href=""><img class="onhover" src="resources/images/components/rtx2080ti.png" width="200"
+                                        height="150"></a><br/>
+                        rtx2080ti
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row-custom-center">
+                        <a href=""><img class="onhover" src="resources/images/pcs/nitron.png" width="150" height="150"></a><br/>
+                        jakiścik nitron
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 mb-4">
-            <div class="card h-100">
-                <h4 class="card-header"><b>Nasz Bank bankiem roku według magazynu "The Money"</b></h4>
-                <div class="card-body">
-                    <p class="card-text">Nasz Bank otrzymał tytuł <q>Bank of the Year in Poland</q> w jednym z
-                        najbardziej prestiżowych konkursów branżowych, organizowanym przez magazyn <q>The Money</q>.
-                        Nagroda potwiedza silną pozycję banku oraz sukces strategicznych projektów zrealizowanych
-                        przez
-                        Nasz Bank.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mb-4">
-            <div class="card h-100">
-                <h4 class="card-header"><b>Nowe <q>Konto Diamond</q></b></h4>
-                <div class="card-body">
-                    <p class="card-text"><q>Konto Diamond</q> zostało stworzone z myślą zarówno o małych, średnich
-                        jak i
-                        dużych przedsiębiorcach. Uruchomiliśmy wraz z tym kontem <b>Private Banking</b>, który ma za
-                        zadanie pomoc właścicielowi konta w zakresie usług bankowych, inwestycyjnych i kredytowych.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /.row -->
-
-    <!-- Portfolio Section -->
-    <h2>Zarząd</h2>
-
-    <div class="row">
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="resources/images/ekipa1.jpg" alt="Prezes"></a>
-                <div class="card-body">
-                    <h4 class="card-title">Andrzej Waligóra</h4>
-                    <p class="card-text">Prezes Zarządu Banku Nasz Bank.</p>
-                    <p>Kieruje bieżącą działalnością Banku i pracami Zarządu Banku. Reprezentuje Zarząd Banku wobec
-                        osób
-                        trzecich. Sprawuje ogólny nadzór nad jednostkami organizacyjnymi Banku. Nadzoruje Obszar
-                        Zarządzania Bankiem, w tym sprawuje nadzór nad ryzykiem braku zgodności działalności Banku z
-                        przepisami prawa, regulacjami wewnętrznymi i standardami rynkowymi.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="resources/images/ekipa2.jpg" alt="Wiceprezes"></a>
-                <div class="card-body">
-                    <h4 class="card-title">
-                        Janusz Kowalski
-                    </h4>
-                    <p class="card-text">Wiceprezes Zarządu Banku.</p>
-                    <p>Nadzoruje Obszar Finansów, w tym Pion Rachunkowości Finansowej, Biuro Zarządzania Jakością
-                        Danych
-                        i Kontroli Operacyjnej Obszaru Rachunkowości, Departament Podatkowy, Pion Zarządzania
-                        Aktywami i
-                        Pasywami, Pion Rachunkowości Zarządczej i Relacji Inwestorskich.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="resources/images/ekipa3.jpg" alt="Wiceprezes"></a>
-                <div class="card-body">
-                    <h4 class="card-title">
-                        Michał Poziomka
-                    </h4>
-                    <p class="card-text">Wiceprezes Zarządu Banku.</p>
-                    <p>Nadzoruje Obszar Strategii i Transformacji Banku, w tym Pion Transformacji Cyfrowej,
-                        Departament
-                        Innowacji, Biuro Koordynacji Projektu Petrus.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="resources/images/ekipa4.jpg" alt="Wiceprezes"></a>
-                <div class="card-body">
-                    <h4 class="card-title">
-                        Wacław Stolarski
-                    </h4>
-                    <p class="card-text">Wiceprezes Zarządu Banku.</p>
-                    <p>Nadzoruje Obszar Bankowości Personal Finance, w tym Pion Sprzedaży Consumer Finance, Pion
-                        Marketingu i Rozwoju PF, Departament Planowania i Finansów PF, Pion Sprzedaży PF, Pion
-                        Operacji
-                        PF.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="resources/images/ekipa5.jpg" alt="Wiceprezes"></a>
-                <div class="card-body">
-                    <h4 class="card-title">
-                        Szymon Kamiński
-                    </h4>
-                    <p class="card-text">Wiceprezes Zarządu Banku.</p>
-                    <p>Nadzoruje obszar Bankowości CIB, w tym Pion Rynków Finansowych, Departament Klientów
-                        Strategicznych CIB, Departament Wsparcia CIB.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="resources/images/ekipa6.jpg" alt="Wiceprezes"></a>
-                <div class="card-body">
-                    <h4 class="card-title">
-                        Tomasz Tryta
-                    </h4>
-                    <p class="card-text">Wiceprezes Zarządu Banku.</p>
-                    <p>Nadzoruje Obszar Bankowości Detalicznej i Biznesowej, w tym Departament Zarządzania
-                        Sprzedażą,
-                        Pion Klientów Detalicznych i Biznesowych, Departament Decyzji Kredytowych, Departament
-                        Planowania Bankowości Detalicznej, Pion Bankowości Internetowej, Departament Bankowości
-                        Prywatnej, Departament Jakości i Projektów Bankowości Detalicznej, Biuro Maklerskie.</p>
-                </div>
+        <div class="col col-4 border border-warning rounded">
+            <h3 class="text-left font-weight-bold">Gorący strzał!</h3>
+            <div class="text-center">
+                <a href=""><img src="resources/images/phones/oneplus7pro.jpg" width="200" height="200"></a>
+                <h4>OnePlus 7 PRO</h4>
+                <h5 class="hot-shot-discount">3000zł</h5>
+                <h2 class="hot-shot font-weight-bold">2000zł</h2>
+                <a href="">
+                    <button class="btn btn-warning">Kup Teraz</button>
+                </a>
+                <h5>Do nastepnego<br> <span style="color: red; font-weight: bold">GORĄCEGO STRZAŁU</span> pozostało</h5>
+                <div id="timer"></div>
             </div>
         </div>
     </div>
 </div>
-<footer class="py-5 bg-dark ">
-    <div class="container">
-        <div class="float-left">
-            <a href="#"><img src="resources/images/phone.png" alt="telefon" style="width: 50px; height: 50px;"></a>
-            <b class="ml-2" style="color: red; font-size: 20px;">666-666-666</b>
+<hr>
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <h3>Tu beda sobie jakies artykuły</h3>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a
+        type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+        of
+        Lorem Ipsum.
         </div>
-        <div class="float-right">
-            <a href="#"><img src="resources/images/facebook.png" alt="facebook" style="width: 50px; height: 50px;"></a>
-            <a href="#"><img src="resources/images/twitter.png" alt="twitter" style="width: 50px; height: 50px;"></a>
-            <a href="#"><img src="resources/images/instagram.png" alt="instagram"
-                             style="width: 50px; height: 50px;"></a>
+        <div class="col">
+            <h3>Tu beda sobie jakies artykuły</h3>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+            a
+            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+            of
+            Lorem Ipsum.
         </div>
-        <p class="m-0 text-center text-white">Copyright &copy; Rafał Wawiórka</p>
+        <div class="col">
+            <h3>Tu beda sobie jakies artykuły</h3>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+            a
+            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+            of
+            Lorem Ipsum.
+        </div>
     </div>
-</footer>
+</div>
+
+<?php require_once('footer.php') ?>
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
@@ -225,6 +175,37 @@
         document.getElementById("menufixer").style.height = height.toString()
     }
 </script>
+<script>
+    var today = new Date();
+    var countDownDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), "23", "59", "59").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("timer").style.fontSize = "40px";
+        document.getElementById("timer").innerHTML = hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+</script>
+
 </body>
 
 </html>
